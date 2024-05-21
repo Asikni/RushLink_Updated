91d60296fbf033a3571b79d4203c3e9b065b40d2
Imran <mohdimranshountoo@gmail.com>
2024-05-21 23:23:03 +0530

    updated code

--- app/LandingPage/Components/PostsPage/Compoents/PostsCard/PostsCard.tsx
+++ app/LandingPage/Components/PostsPage/Compoents/PostsCard/PostsCard.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 export const PostsCard = () => {
   return (
     <div className="cards-inner-container">
-      <div className="cards">
+      <div className="cards-inner-container-all-cards">
         {CardsData.map((card, index) => (
           <div className="individual-card" key={index}>
-            <div className="card-top-details">
-              <div className="university-logo">
+            <div className="individual-card-top-details">
+              <div className="individual-card-top-details-university-logo">
                 <Image
                   src={card.universityLogo}
                   width={52}
@@ -17,52 +17,62 @@ export const PostsCard = () => {
                 />
               </div>
               <div>
-                <div className="card-top-details-post-meta">
+                <div className="individual-card-top-details-post-meta">
                   <p className="post-meta-post-by">{card.postBy}</p>
                   <p className="university-post-container-university-name">
                     {card.universityName}
                   </p>
                 </div>
+                <div className="individual-card-top-details-tags">
+                  <p>{card.tagOne}</p>
+                  <p>{card.tagTwo}</p>
+                  <p>{card.tagThree}</p>
+                  <p>{card.others}</p>
+                </div>
               </div>
             </div>
-            <h2 className="card-top-heading">{card.cardHeading}</h2>
+            {/* <h2 className="card-top-heading">{card.cardHeading}</h2> */}
             <h3 className="card-comment">{card.cardComments}</h3>
             <div className="card-footer">
-              <div className="by-date">
-                <p>{card.cardBy}</p>
-                <p>{card.dateTime}</p>
-              </div>
               <div className="comments-data">
-                <div>
-                  <Image
-                    className="viewsLogo"
-                    src={card.viewsLogo}
-                    width={28}
-                    height={21}
-                    alt="views logo"
-                  />
+                <div className="comments-data-left-container">
+                  <div
+                    className="comments-logo"
+                    style={{ backgroundImage: `url(${card.commentsLogo})` }}
+                  ></div>
+                  <p className="comments">{card.comments}</p>
+
+                  <div
+                    className="views-logo"
+                    style={{ backgroundImage: `url(${card.viewsLogo})` }}
+                  ></div>
+                  <p className="views">{card.views}</p>
+
+                  <div
+                    className="shared-posts-logo"
+                    style={{ backgroundImage: `url(${card.sharedPostsLogo})` }}
+                  ></div>
+                  <p className="shared-posts">{card.sharedPosts}</p>
+
+                  <div
+                    className="saved-posts-logo"
+                    style={{ backgroundImage: `url(${card.savedPostsLogo})` }}
+                  ></div>
+                  <p className="saved-posts">{card.savedPosts}</p>
                 </div>
-                <p className="views">{card.views}</p>
-                <div>
-                  <Image
+                <div className="comments-data-right-container">
+                  {" "}
+                  <div
                     className="likes-logo"
-                    src={card.likesLogo}
-                    width={28}
-                    height={21}
-                    alt="likes logo"
-                  />
-                </div>
-                <p className="likes">{card.likes}</p>
-                <div>
-                  <Image
+                    style={{ backgroundImage: `url(${card.likesLogo})` }}
+                  ></div>
+                  <p className="likes">{card.likes}</p>
+                  <div
                     className="dislikes-logo"
-                    src={card.dislikesLogo}
-                    width={28}
-                    height={21}
-                    alt="dislikes logo"
-                  />
+                    style={{ backgroundImage: `url(${card.dislikesLogo})` }}
+                  ></div>
+                  <p className="dislikes">{card.dislikes}</p>
                 </div>
-                <p className="dislikes">{card.dislikes}</p>
               </div>
             </div>
           </div>
